e4571b463d99bf94ad004ed3b742f8c3dcfc00b7
aaryan2403 <77224473+aaryan2403@users.noreply.github.com>
2026-08-14 17:05:33 -0400

    Remove AnnouncementBar from layout
    Removed AnnouncementBar component from layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,6 @@ export default function RootLayout({
       >
         <ThemeProvider>
           <ShopProvider>
-            <AnnouncementBar />
             <Navbar />
             <NavbarSpacer />
             <SearchOverlay />
